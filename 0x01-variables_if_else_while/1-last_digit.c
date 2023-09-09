784f0c639fd6dc0ce6a8209250fee4fc0b81cdeb
Lina <elamrani661@gmail.com>
2023-09-09 16:08:23 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ int main(void)
 
 	if (n > 5)
 	{	
-		printf("%d and is greater than 5\n", n);
+		printf("%d the string  and is greater than 5\n", n);
 	}
-	else if (n == 0)
+	else if (n < 5)
 	{
-		printf("%d and is 0\n", n);
+		printf("%d the string and is less than 6 and not 0\n", n);
 	}
 	else 
 	{
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("%d the string and is 0\n", n);
 	}
 	return (0);
 }
