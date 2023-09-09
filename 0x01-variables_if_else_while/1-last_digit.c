781ab5f7e468bd71fefab206aa67a4147f8dd860
Lina <elamrani661@gmail.com>
2023-09-09 16:48:32 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -21,16 +21,16 @@ int main(void)
 	ld = n % 10;
 
 	if (ld > 5)
-	{	
-		printf("last digit of %d is %d and is greater than 5\n", n, ld);
+	{
+                printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
 	else if (ld < 6 && ld != 0)
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
-	else 
+	else
 	{
-		printf("last digit of %d is %d and is 0\n", n, ld);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
 	return (0);
 }
